f537f50569e26c9982df92be84296e2b7efd03ca
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-20 22:08:21 +0800

    refactor: add initial content

--- pages/stripe-checkout/getting-started.vue
+++ pages/stripe-checkout/getting-started.vue
@@ -2,13 +2,35 @@
   v-container
     v-row(justify="center")
       v-col(cols="12" md="10")
-        h1 Getting Started
+        h1.mb-5 Getting Started
+        v-card
+          v-card-text
+            p Vue Stripe Checkout is an easy to implement, and well maintained Vue.js plugin for Stripe Checkout.
+      v-col(cols="12" md="10")
+        h1.mb-5 Installation
+        v-card
+          v-card-text
+            p Vue Stripe Checkout is available on NPM, or Yarn.
+            h3 NPM
+            code-snip(lang="language-bash") npm install @vue-stripe/vue-stripe
+            h3 Yarn
+            code-snip(lang="language-bash") yarn add @vue-stripe/vue-stripe
+      v-col(cols="12" md="10")
+        h1.mb-5 Usage
+        v-card
+          v-card-text
+            h3 Import
+            code-snip(lang="language-javascript") import { StripeCheckout } from '@vue-stripe/vue-stripe';
 </template>
 
 <script>
 import headMeta from '~/utils/head-meta';
+import CodeSnip from '~/components/commons/code-snippet';
 export default {
   layout: 'docs',
+  components: {
+    CodeSnip,
+  },
   head () {
     return headMeta({
       title: 'Getting Started - Vue Stripe Checkout',
