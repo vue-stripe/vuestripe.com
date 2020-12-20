61eab5ec1a580c24486c85c971184e3d175fee3a
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-20 21:20:13 +0800

    feat: create stripe element faqs page

---
+++ pages/stripe-checkout/faqs.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-container
+    v-row(justify="center")
+      v-col(cols="12" md="10")
+        h1 FAQs
+</template>
+
+<script>
+import headMeta from '~/utils/head-meta';
+export default {
+  layout: 'docs',
+  head () {
+    return headMeta({
+      title: 'FAQs - Vue Stripe Checkout',
+      description: 'A guide on how to install vue stripe checkout',
+      socialBanner: require('../../assets/vue-stripe-logo-variant-1-small.png'),
+    });
+  },
+};
+</script>
