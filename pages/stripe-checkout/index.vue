18600ca9bf6d441108ca52b307dbbbdb379a055b
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-20 21:20:26 +0800

    feat: create stripe element index page

---
+++ pages/stripe-checkout/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  layout: 'docs',
+  created () {
+    this.$router.push({ name: 'stripe-checkout-getting-started' });
+  },
+};
+</script>
