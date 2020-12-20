d2e0f2541c90906357fcea34e34dc051349390e5
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-20 21:21:52 +0800

    feat: create stripe ekenebts index page

---
+++ pages/stripe-elements/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  layout: 'docs',
+  created () {
+    this.$router.push({ name: 'stripe-elements-getting-started' });
+  },
+};
+</script>
