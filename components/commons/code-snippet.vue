5ec609c66bde7a64fb5f4640f9d0791858db5575
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-20 22:08:00 +0800

    feat: create code snippet component

---
+++ components/commons/code-snippet.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  pre
+    code(:class="lang").elevation-0
+      slot(name="default")
+</template>
+
+<script>
+import Prism from '~/plugins/prism';
+export default {
+  props: {
+    lang: {
+      type: String,
+    },
+  },
+  mounted () {
+    Prism.highlightAll();
+  },
+};
+</script>
+
+<style scoped>
+pre, code {
+  font-family: 'JetBrains Mono', monospace !important;
+  font-size: 16px;
+}
+</style>
