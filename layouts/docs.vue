1f1638fe462993ba74687d2b4aeecf719ac9d13b
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-20 21:19:38 +0800

    feat: create docs layout

---
+++ layouts/docs.vue
@@ -0,0 +1,97 @@
+<template lang="pug">
+  v-app#app
+    v-app-bar(app)
+    v-navigation-drawer(
+      v-model="drawer"
+      app
+    )
+      v-toolbar(flat)
+        img(width="45" src="~/assets/vue-stripe-logo-variant-1-small.png")
+        h2 Vue #[span.primary--text Stripe]
+      v-list
+        v-list-item(to="/")
+          v-list-item-content
+            v-list-item-title Home
+        v-list-group(:value="true")
+          template(v-slot:activator)
+            v-list-item-title Vue Stripe Checkout
+          template(v-for="menu in stripeCheckoutMenus")
+            v-list-item(:to="{ name: menu.route }")
+              v-list-item-content
+                v-list-item-title.pl-3 {{menu.name}}
+        v-list-group(:value="true")
+          template(v-slot:activator)
+            v-list-item-title Vue Stripe Elements
+          template(v-for="menu in stripeElementsMenus")
+            v-list-item(:to="{ name: menu.route }")
+              v-list-item-content
+                v-list-item-title.pl-3 {{menu.name}}
+    v-content
+      nuxt
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: null,
+      stripeCheckoutMenus: [
+        {
+          name: 'Getting Started',
+          route: 'stripe-checkout-getting-started',
+        },
+        {
+          name: 'Nuxt.js',
+          route: 'stripe-checkout-nuxt',
+        },
+        {
+          name: 'One-time Payment',
+          route: 'stripe-checkout-one-time-payment',
+        },
+        {
+          name: 'Subscription (Recurring Payment)',
+          route: 'stripe-checkout-recurring-payment',
+        },
+        {
+          name: 'Sessions',
+          route: 'stripe-checkout-sessions',
+        },
+        {
+          name: 'FAQs',
+          route: 'stripe-checkout-faqs',
+        },
+        {
+          name: 'Change Log',
+          route: 'stripe-checkout-change-log',
+        },
+      ],
+      stripeElementsMenus: [
+        {
+          name: 'Getting Started',
+          route: 'stripe-elements-getting-started',
+        },
+        {
+          name: 'FAQs',
+          route: 'stripe-elements-faqs',
+        },
+        {
+          name: 'Change Log',
+          route: 'stripe-elements-change-log',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Sniglet', cursive;
+  /* font-family: 'JetBrains Mono', monospace; */
+  color: #34495E;
+}
+
+h1,h2,h3,h4,h5,h6 {
+  color: #34495E;
+}
+</style>
