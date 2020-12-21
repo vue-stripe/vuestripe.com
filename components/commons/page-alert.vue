e7a00b22d18008fb0add01fb47c2a7f21d6059f0
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-21 12:05:14 +0800

    feat: create page-alert component

---
+++ components/commons/page-alert.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  v-alert(
+    border="left"
+    colored-border
+    color="primary"
+    elevation="2"
+  )
+    v-row(align="center")
+      v-col.grow
+        | {{content}}
+      v-col.shrink
+        nice
+</template>
+
+<script>
+import Nice from '~/components/commons/nice';
+export default {
+  components: {
+    Nice,
+  },
+  props: {
+    content: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
