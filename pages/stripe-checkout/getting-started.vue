7d2ed4ea04cd539220107e26407d05adc1e2baf3
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-21 12:05:34 +0800

    refactor: update getting-started content

--- pages/stripe-checkout/getting-started.vue
+++ pages/stripe-checkout/getting-started.vue
@@ -3,9 +3,7 @@
     v-row(justify="center")
       v-col(cols="12" md="10")
         h1.mb-5 Getting Started
-        v-card
-          v-card-text
-            p Vue Stripe Checkout is an easy to implement, and well maintained Vue.js plugin for Stripe Checkout.
+        page-alert(content="Vue Stripe Checkout is an easy to implement, and well maintained Vue.js plugin for Stripe Checkout.")
       v-col(cols="12" md="10")
         h1.mb-5 Installation
         v-card
@@ -20,16 +18,24 @@
         v-card
           v-card-text
             h3 Import
-            code-snip(lang="language-javascript") import { StripeCheckout } from '@vue-stripe/vue-stripe';
+            p You just need to import the #[code StripeCheckout] component wherever it is needed, and Stripe SDK will only be loaded by the time the component has been mounted.
+            code-snip(lang="language-html") {{usageSnippet}}
 </template>
 
 <script>
 import headMeta from '~/utils/head-meta';
 import CodeSnip from '~/components/commons/code-snippet';
+import PageAlert from '~/components/commons/page-alert';
+import usageSnippet from '~/assets/snippets/stripe-checkout/getting-started-usage.md';
 export default {
   layout: 'docs',
   components: {
     CodeSnip,
+    PageAlert,
+  },
+  data () {
+    this.usageSnippet = usageSnippet;
+    return {};
   },
   head () {
     return headMeta({
