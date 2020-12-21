cbac5770f2b979249f9511ff5371a1483b40d61d
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-21 12:04:53 +0800

    refactor: add nice button

---
+++ components/commons/nice.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  div
+    v-btn(
+      color="success"
+      depressed
+      outlined
+      @click="nice"
+    ).text-none Nice!
+    v-snackbar(
+      v-model="showSnack"
+      :timeout="3000"
+      color="success"
+      top
+      center
+    ) Nice :)
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showSnack: false,
+    };
+  },
+  methods: {
+    nice () {
+      this.showSnack = true;
+    },
+  },
+};
+</script>
