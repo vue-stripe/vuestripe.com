914fa4d0187eaa6ef05acd55ad7ecdd88f4ff026
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-22 03:41:02 +0800

    feat: create icon link component

---
+++ components/commons/icon-link.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  v-icon(
+    :href="link"
+    @click="go"
+  ).primary--text.icon-link {{icon}}
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: 'mdi-open-in-new',
+    },
+  },
+  methods: {
+    go () {
+      window.open(this.link, '_blank', 'noopener noreferrer');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.icon-link {
+  font-size: 15px;
+}
+
+.icon-link:hover,
+.icon-link:active,
+.icon-link:focus {
+  cursor: pointer;
+}
+</style>
