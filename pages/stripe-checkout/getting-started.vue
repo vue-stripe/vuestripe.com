44b7b190707a115b14e04236b653677cee6a98c5
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-22 03:41:28 +0800

    refactor: add table for props, and events

--- pages/stripe-checkout/getting-started.vue
+++ pages/stripe-checkout/getting-started.vue
@@ -20,29 +20,89 @@
             h3 Import
             p You just need to import the #[code StripeCheckout] component wherever it is needed, and Stripe SDK will only be loaded by the time the component has been mounted.
             code-snip(lang="language-html") {{usageSnippet}}
+      v-col(cols="12" md="10")
+        h1.mb-5 Props
+        v-card
+          v-card-text
+            div(style="overflow-x: auto;")
+              table(width="100%")
+                thead
+                  tr(align="left")
+                    th Name
+                    th Type
+                    th Default
+                    th Required
+                    th Description
+                tbody
+                  template(v-for="prop in checkoutProps")
+                    tr
+                      td
+                        span.mr-1 {{prop.name}}
+                        icon-link(
+                          v-if="prop.link"
+                          :link="prop.link"
+                        )
+                      td {{prop.type}}
+                      td {{prop.default}}
+                      td {{prop.required}}
+                      td(v-html="prop.description")
+      v-col(cols="12" md="10")
+        h1.mb-5 Events
+        v-card
+          v-card-text
+            div(style="overflow-x: auto;")
+              table(width="100%")
+                thead
+                  tr(align="left")
+                    th Name
+                    th Type
+                    th Description
+                tbody
+                  template(v-for="event in checkoutEvents")
+                    tr
+                      td {{event.name}}
+                      td {{event.type}}
+                      td(v-html="event.description")
 </template>
 
 <script>
 import headMeta from '~/utils/head-meta';
 import CodeSnip from '~/components/commons/code-snippet';
 import PageAlert from '~/components/commons/page-alert';
+import IconLink from '~/components/commons/icon-link';
 import usageSnippet from '~/assets/snippets/stripe-checkout/getting-started-usage.md';
+import checkoutProps from '~/assets/fixtures/checkout-props.json';
+import checkoutEvents from '~/assets/fixtures/checkout-events.json';
 export default {
   layout: 'docs',
   components: {
     CodeSnip,
     PageAlert,
+    IconLink,
   },
   data () {
     this.usageSnippet = usageSnippet;
+    this.checkoutProps = checkoutProps;
+    this.checkoutEvents = checkoutEvents;
     return {};
   },
   head () {
     return headMeta({
       title: 'Getting Started - Vue Stripe Checkout',
       description: 'A guide on how to install vue stripe checkout',
-      socialBanner: require('../../assets/vue-stripe-logo-variant-1-small.png'),
+      socialBanner: require('../../assets/images/vue-stripe-logo-variant-1-small.png'),
     });
   },
 };
 </script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+
+table, th, td {
+  border: 1px solid lightgrey;
+  padding: 10px;
+}
+</style>
