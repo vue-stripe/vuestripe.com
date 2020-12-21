182c501518c4363e9b2030b41a045eacb4827ee5
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-22 03:41:13 +0800

    refactor: add comment

--- pages/stripe-checkout/change-log.vue
+++ pages/stripe-checkout/change-log.vue
@@ -3,6 +3,7 @@
     v-row(justify="center")
       v-col(cols="12" md="10")
         h1 Change Log
+        //- button slot removed
 </template>
 
 <script>
@@ -13,7 +14,7 @@ export default {
     return headMeta({
       title: 'Change Log - Vue Stripe Checkout',
       description: 'A guide on how to install vue stripe checkout',
-      socialBanner: require('../../assets/vue-stripe-logo-variant-1-small.png'),
+      socialBanner: require('../../assets/images/vue-stripe-logo-variant-1-small.png'),
     });
   },
 };
