1bd3d99e5d16ddaa5f18c06ba433f871af31842a
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-25 03:11:57 +0800

    refactor: update meta title for elements change log page

--- pages/stripe-elements/change-log.vue
+++ pages/stripe-elements/change-log.vue
@@ -11,7 +11,7 @@ export default {
   layout: 'docs',
   head () {
     return headMeta({
-      title: 'Change Log - Vue Stripe Checkout',
+      title: 'Change Log - Vue Stripe Elements',
       description: 'A guide on how to install vue stripe checkout',
       socialBanner: require('../../assets/images/vue-stripe-logo-variant-1-small.png'),
     });
