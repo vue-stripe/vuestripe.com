b9e5450c4512df1e1904d20b0cacbdc9c0e0f452
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-12-25 03:11:41 +0800

    feat: create elements card page

---
+++ pages/stripe-elements/card.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  v-container
+    v-row(justify="center")
+      v-col(cols="12" md="10")
+        h1.mb-1 Card
+        pre Documenation work in progress
+</template>
+
+<script>
+import headMeta from '~/utils/head-meta';
+export default {
+  layout: 'docs',
+  head () {
+    return headMeta({
+      title: 'Card - Vue Stripe Elements',
+      description: 'How to use Stripe Elements Card in Vue',
+      socialBanner: require('../../assets/images/vue-stripe-logo-variant-1-small.png'),
+    });
+  },
+};
+</script>
