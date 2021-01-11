36cb64d944fd5f65b9276fdf7589cf76607cfb82
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-11 11:53:18 +0800

    feat: create ad component

---
+++ components/commons/ads.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div(
+    :style="{ height, width }"
+  )
+    adsbygoogle(
+      :ad-slot="adSlot"
+    ).pink
+</template>
+
+<script>
+export default {
+  props: {
+    adSlot: {
+      type: String,
+    },
+  },
+  computed: {
+    height () {
+      return this.$isMobile ? '280px' : '280px';
+    },
+    width () {
+      return this.$isMobile ? '320px' : '727px';
+    },
+  },
+};
+</script>
