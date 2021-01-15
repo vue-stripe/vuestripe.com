895fda2d82e95c74e40210cd729523b6a04458ff
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-01-16 04:24:13 +0800

    feat: create component for carbon ads

---
+++ components/commons/carbon-ads.vue
@@ -0,0 +1,99 @@
+<script>
+export default {
+  name: 'CarbonAds',
+  watch: {
+    $route (to, from) {
+      if (
+        to.path !== from.path &&
+        // Only reload if the ad has been loaded
+        // otherwise it's possible that the script is appended but
+        // the ads are not loaded yet. This would result in duplicated ads.
+        this.$el.querySelector('#carbonads')
+      ) {
+        this.$el.innerHTML = '';
+        this.load();
+      }
+    },
+  },
+  mounted () {
+    if (process.env.NODE_ENV === 'production') {
+      this.load();
+    }
+  },
+  methods: {
+    load () {
+      const caServe = process.env.CA_SERVE;
+      const caPlacement = process.env.CA_PLACEMENT;
+      const script = document.createElement('script');
+      script.id = '_carbonads_js';
+      script.src = `//cdn.carbonads.com/carbon.js?serve=${caServe}&placement=${caPlacement}`;
+      this.$el.appendChild(script);
+    },
+  },
+  render (h) {
+    return h('div', { class: 'carbon-ads' });
+  },
+};
+</script>
+
+<style scoped>
+#carbonads {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu,
+  Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#carbonads {
+  display: flex;
+  max-width: 330px;
+  background-color: hsl(0, 0%, 98%);
+  box-shadow: 0 1px 4px 1px hsla(0, 0%, 0%, .1);
+}
+
+#carbonads a {
+  color: inherit;
+  text-decoration: none;
+}
+
+#carbonads a:hover {
+  color: inherit;
+}
+
+#carbonads span {
+  position: relative;
+  display: block;
+  overflow: hidden;
+}
+
+#carbonads .carbon-wrap {
+  display: flex;
+}
+
+.carbon-img {
+  display: block;
+  margin: 0;
+  line-height: 1;
+}
+
+.carbon-img img {
+  display: block;
+}
+
+.carbon-text {
+  font-size: 13px;
+  padding: 10px;
+  line-height: 1.5;
+  text-align: left;
+}
+
+.carbon-poweredby {
+  display: block;
+  padding: 8px 10px;
+  background: repeating-linear-gradient(-45deg, transparent, transparent 5px, hsla(0, 0%, 0%, .025) 5px, hsla(0, 0%, 0%, .025) 10px) hsla(203, 11%, 95%, .4);
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: .5px;
+  font-weight: 600;
+  font-size: 9px;
+  line-height: 1;
+}
+</style>
