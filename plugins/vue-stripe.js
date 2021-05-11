efa899e948b28859164ca91c7c14c98762f1ba39
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-05-11 16:07:30 +0800

    chore: remove comments

--- plugins/vue-stripe.js
+++ plugins/vue-stripe.js
@@ -3,12 +3,10 @@ import {
   StripePlugin,
   StripeCheckout,
   StripeElementCard,
-  // StripeElementsPlugin,
 } from '@vue-stripe/vue-stripe';
 
 export default () => {
   Vue.component('StripeCheckout', StripeCheckout);
   Vue.component('StripeElementCard', StripeElementCard);
   Vue.use(StripePlugin, { pk: process.env.STRIPE_PK });
-  // Vue.use(StripeElementsPlugin, { pk: process.env.STRIPE_PK });
 };
