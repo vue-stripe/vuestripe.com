8dde5861f5084a8e645725cb78433757d433648b
Jofferson R Tiquez <jofftiquez@gmail.com>
2022-01-15 19:06:40 +0800

    refactor: import StripeElementPayment

--- plugins/vue-stripe.js
+++ plugins/vue-stripe.js
@@ -3,10 +3,12 @@ import {
   StripePlugin,
   StripeCheckout,
   StripeElementCard,
+  StripeElementPayment,
 } from '@vue-stripe/vue-stripe';
 
 export default () => {
   Vue.component('StripeCheckout', StripeCheckout);
   Vue.component('StripeElementCard', StripeElementCard);
+  Vue.component('StripeElementPayment', StripeElementPayment);
   Vue.use(StripePlugin, { pk: process.env.STRIPE_PK });
 };
