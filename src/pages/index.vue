23faa3d95921519fec1447a5075421739421dccb
Joff Tiquez <jofftiquez@gmail.com>
2023-08-04 00:20:26 +0800

    🎨 style(index.vue): update text color of "Stripe" to use the primary color for better visual consistency 🔧 chore(index.vue): update metaTitle and metaDescription for improved SEO and branding The text color of "Stripe" in the heading has been updated to use the primary color, providing better visual consistency with the rest of the design. The metaTitle and metaDescription have been updated to improve SEO and branding. The new metaTitle is "Home - Vue Stripe" and the new metaDescription is "Vue.js components and composables for Stripe Checkout, Elements, and more."

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,33 @@
   <main class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-2xl">
-        <h1 data-aos="fade-down" class="text-4xl md:text-7xl font-bold mb-6"><span>Vue</span> <span class="text-stripe-purple">Stripe</span> 💳</h1>
-        <h1 data-aos="fade-down" class="text-4xl md:text-4xl font-bold mb-6">Stripe Checkout & Elements for Vue.js</h1>
-        <nuxt-link class="btn btn-primary" to="/docs">Get Started</nuxt-link>
+        <h1 data-aos="fade-down" class="text-4xl md:text-7xl font-bold mb-6"><span>Vue</span> <span class="text-primary">Stripe</span> 💳</h1>
+        <p data-aos="fade-down" class="text-xl md:text-2xl font-medium mb-6">
+          {{metaDescription}}
+        </p>
+        <div class="flex justify-center gap-2">
+          <nuxt-link
+            class="btn btn-primary"
+            to="/docs"
+          >
+            Get Started
+            <i class="las la-play text-3xl"></i>
+          </nuxt-link>
+          <nuxt-link
+            class="btn hover:btn-secondary btn-outline"
+            to="/docs/components/address-element"
+          >
+            Components
+            <i class="las la-cubes text-3xl"></i>
+          </nuxt-link>
+          <nuxt-link
+            class="btn hover:btn-secondary btn-outline"
+            to="/docs/composables/use-stripe"
+          >
+            Composables
+            <i class="las la-plug text-3xl"></i>
+          </nuxt-link>
+        </div>
       </div>
     </div>
   </main>
@@ -22,8 +46,8 @@ export default {
       layout: 'default',
     });
 
-    const metaTitle = 'Home - Joff Tiquez';
-    const metaDescription = 'Web Developer from 🇵🇭';
+    const metaTitle = 'Home - Vue Stripe';
+    const metaDescription = 'Vue.js components and composables for Stripe Checkout, Elements, and more.';
 
     useSeoMeta({
       title: metaTitle,
@@ -39,6 +63,7 @@ export default {
 
     return {
       socialLinks: SOCIAL_LINKS,
+      metaDescription,
     };
   },
 };
