b2a8809b59a17eee28aa28c775b6ea3af8a5e3a2
Joff Tiquez <jofftiquez@gmail.com>
2023-08-04 00:21:42 +0800

    ✨ feat(docs): add new components Address Element, Express Checkout Element, Link Authentication Element, and Payment Element The commit adds four new Vue components: Address Element, Express Checkout Element, Link Authentication Element, and Payment Element. These components are part of the documentation pages for Vue Stripe Components. Each component defines its own page meta, including title and description, and uses the `useSeoMeta` and `definePageMeta` functions from the `#imports` module to set the SEO metadata. This addition enhances the documentation by providing examples and explanations for these specific components.

---
+++ src/pages/docs/components/link-authentication-element.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Link Authentication Element</h1>
+  </main>
+</template>
+
+<script>
+// import { onMounted, ref } from 'vue';
+import { useSeoMeta, definePageMeta } from '#imports';
+export default {
+  setup () {
+    definePageMeta({
+      layout: 'blank',
+    });
+
+    const metaTitle = 'Link Authentication Element - Vue Stripe Components';
+    const metaDescription = 'Link Authentication Element for Vue Stripe Components.';
+
+    useSeoMeta({
+      title: metaTitle,
+      ogTitle: metaTitle,
+      description: metaDescription,
+      ogDescription: metaDescription,
+      // image: ogBanner,
+      // ogImage: ogBanner,
+      // url: 'https://jofftiquez.com/gui',
+      // ogUrl: 'https://jofftiquez.com/gui',
+      twitterCard: 'summary_large_image',
+    });
+  },
+};
+</script>
